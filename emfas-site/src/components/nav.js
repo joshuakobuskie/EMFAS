342d5746d6a9bc474c75e43b9c06602bc5a9aeb6
Bill Kaminski <billykaminski123@gmail.com>
2023-03-18 15:54:47 -0400

    added navbar responsivness (still WIP)

--- emfas-site/src/components/nav.js
+++ emfas-site/src/components/nav.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import logo from '../emfas_logo.png';
+import {FaBars, FaTimes} from "react-icons/fa";
+import { useRef } from 'react';
 import './nav.css';
 
 export default function Navbar() {
+  const navRef = useRef();
+
+  const showNav = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
   return (
     
-    <nav className="nav">
+    <header className="nav">
      
-      <img src={logo} className="Nav-logo"/> 
+      <img src={logo} className="nav-logo"/> 
+
       <div className="rect1">&nbsp;</div>
       <div className="rect2">&nbsp;</div>
       <div className="rect3">&nbsp;</div>
-      <ul className="links">  
-        <a href="/Home"> Home</a>
-        <a href="/Donate"> Donate</a>
-        <a href="/Join">Join the Squad</a>
-        <a href="/Officers"> Officers</a>
-      </ul>
+ 
+      <button className="nav-btn nav-bars" onClick={showNav}>
+          <FaBars/>
+      </button>
+
+      <nav className="links" ref={navRef}>  
+        <a href="/#"> Home</a>
+        <a href="/#"> Donate</a>
+        <a href="/#">Join the Squad</a>
+        <a href="/#"> Officers</a>
+
+        <button className="nav-btn nav-close-btn" onClick={showNav}>
+          <FaTimes/>
+        </button>
+       
+      </nav>
+     
         
-    </nav>
+    </header>
   )
 }
