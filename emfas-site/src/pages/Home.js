195e621bfb0a426c061417ddf4264e0ab697d3cb
Bill Kaminski <billykaminski123@gmail.com>
2023-03-23 14:28:08 -0400

    homepage title and stats added

--- emfas-site/src/pages/Home.js
+++ emfas-site/src/pages/Home.js
@@ -1,4 +1,37 @@
+import "./Home.css";
+import truck from "../truck.png";
+
+
 export default function Home(){
-    return<h1>Home</h1>
+    return(
+        <div className="block1">
+            <div className="title-stats">
+                <h2>Englishtown Manalapan</h2>
+                <h1>First Aid Squad</h1>
+                <div className="stats">
+
+                <table className="stats-table">
+                    <tr>
+                        <td>Avg Response Time</td>
+                        <td>Total Calls Taken</td>
+                        <td>Miles Driven</td>
+                    </tr>
+                    <tr>
+                        <th>-- min</th>
+                        <th>-- calls</th>
+                        <th>-- miles</th>
+                    </tr>
+                </table>
+            
+                </div>
+                
+            </div>
+            <div className= "truck-img">
+                <img src={truck} className="truck"/> 
+            </div>
+        </div>
+
+
+    );
 
 }
