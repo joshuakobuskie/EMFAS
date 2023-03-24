dcc1ba9168f6a9fbcd82b516b7ccfb32fe6f03fe
wck3 <wck3@njit.edu>
2023-03-23 23:13:22 -0400

    general responsivness tweaks, navbar changed on zoom

--- emfas-site/src/pages/Home.js
+++ emfas-site/src/pages/Home.js
@@ -13,20 +13,20 @@ export default function Home(){
                 <div className="title-stats">
                     <h2>Englishtown Manalapan</h2>
                     <h1>First Aid Squad</h1>
-                    <div className="stats">
-                        <table className="stats-table">
-                            <tr>
-                                <td>Avg Response Time</td>
-                                <td>Total Calls Taken</td>
-                                <td>Miles Driven</td>
-                            </tr>
-                            <tr>
-                                <th>-- min</th>
-                                <th>-- calls</th>
-                                <th>-- miles</th>
-                            </tr>
-                        </table>
-                    </div>
+                    
+                    <table className="stats-table">
+                        <tr>
+                            <td>Avg Response Time</td>
+                            <td>Total Calls Taken</td>
+                            <td>Miles Driven</td>
+                        </tr>
+                        <tr>
+                            <th>-- min</th>
+                            <th>-- calls</th>
+                            <th>-- miles</th>
+                        </tr>
+                    </table>
+                  
 
                 </div>
                {/*  <div className= "truck-img"> 
