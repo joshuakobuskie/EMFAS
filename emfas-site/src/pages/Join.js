d51ab4668e2470408e9f25e3c51dce2f5c5f44e8
ct32 <ct32@njit.edu>
2023-03-26 00:05:06 -0400

    fixed overlapping

--- emfas-site/src/pages/Join.js
+++ emfas-site/src/pages/Join.js
@@ -1,11 +1,28 @@
 import Accordion from "../components/accordion";
+import Join_form  from "../components/join_form";
+import JoinCSS from "./Join.css"
+
+
+
+
 
 
 export default function Join(){
     return(
-        <div className="join-info">
-            <Accordion/>
+     <div className="join-container">  
+        <div className={JoinCSS.join_title}>
+            <h1>Join Us!</h1>
         </div>
+            <div className={JoinCSS.join_info}>           
+                <Accordion></Accordion>                   
+            </div>
+            <div className={JoinCSS.join_form}>           
+                <Join_form></Join_form>    
+            </div>
+                 
+        
+    </div>   
+       
     )
 
 };
