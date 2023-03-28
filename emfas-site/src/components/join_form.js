1859a2fc9bd1041a5baa840065d72953df2c219d
ct32 <ct32@njit.edu>
2023-03-28 13:43:27 -0400

    controlled form

--- emfas-site/src/components/join_form.js
+++ emfas-site/src/components/join_form.js
@@ -1,31 +1,53 @@
 import Join_formCSS from './join_form.css';
+import { useState } from 'react';
 
-export default function Join_form(){
+
+const Join_form=()=>{
+    const [name, setName]= useState('');
+    const [age, setAge]= useState('');
+    const [phone, setPhone]= useState('');
+    const [email, setEmail]= useState('');
+
+    //const [showAlert, setShowAlert] = useState(false);
+
+
+    function handleClick(e) {
+        e.preventDefault();
+        if (name && age && phone && email != ''){
+            alert('Your information has been submitted!')
+        }
+        else{
+            alert('You must fill the form to submit.')
+        }
+      }
+
     return(
         <div className={Join_formCSS.container}>
-            <form>
+            <form >
                 <div className="field">
                   <h2><label>Name</label></h2>  
-                    <input type="text" name="name" ></input>
+                    <input type="text" required value={name} onChange={(e)=> setName(e.target.value)} ></input>
                 </div>
                 
                     <div className="sameline">
                       <h2><label >Age</label></h2>
-                        <input type="text" name="age" ></input>
+                        <input type="text" required value={age} onChange={(e)=> setAge(e.target.value)} ></input>
                    
                        <h2><label>Phone Number</label></h2>
-                        <input type="phone" name="phone" ></input>
+                        <input type="phone" required value={phone} onChange={(e)=> setPhone(e.target.value)}  ></input>
                     </div>
                
                 <div className="field">
                  <h2><label>Email</label></h2>
-                    <input type="email" name="email" ></input>
+                    <input type="email" required value={email} onChange={(e)=> setEmail(e.target.value)} ></input>
                 </div>
                 
-                <button className='button'>Submit</button>
+                <button  onClick={handleClick} className='button'>Submit</button>
                
             </form>
         </div>
     )
 
 }
+
+export default Join_form;
